92edf3ceccff958ac7bcc70d79d19ac9da7b994d
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-04-25 01:13:28 +0100

    Add base template structure & static files setup

--- kitchen_manager/settings.py
+++ kitchen_manager/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "restaurant.context_processors.cfg_assets_root"
             ],
         },
     },
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ASSETS_ROOT = "/static/assets"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
