8e363111e20173d5af699014947a7b113a4554ab
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-04-27 00:54:24 +0100

    Add CookSearchForm for filtering cooks by username

---
+++ restaurant/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username"}
+        )
+    )
