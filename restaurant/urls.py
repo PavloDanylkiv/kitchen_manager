bdbc08d0b2fb9ed93f9e3b40589ec3186277a897
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-04-27 01:05:13 +0100

    Add CookListView with search functionality, URL routing & template

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import IndexView
+from .views import IndexView, CookListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("cooks/", CookListView.as_view(), name="cook-list")
 ]
 
 app_name = "restaurant"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
+from django.views import generic
 from django.views.generic import TemplateView
 
+from restaurant.forms import CookSearchForm
 from restaurant.models import DishType, Cook, Dish
 
 
@@ -12,3 +14,24 @@ class IndexView(TemplateView):
         context["num_cook"] = Cook.objects.count()
         context["num_dish"] = Dish.objects.count()
         return context
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    template_name = "restaurant/cook_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all()
+        username = self.request.GET.get("username")
+        if username:
+            return queryset.filter(username__icontains=username)
+        return queryset
