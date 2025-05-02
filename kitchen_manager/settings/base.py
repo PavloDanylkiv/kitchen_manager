229ee67ce196aa5018aff4df5b518951a2b17eeb
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-05-02 18:31:06 +0100

    Change root path to restaurant/ &d set LOGIN_REDIRECT_URL to '/'

--- kitchen_manager/settings/base.py
+++ kitchen_manager/settings/base.py
@@ -128,7 +128,7 @@ ASSETS_ROOT = "/static/assets"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/restaurant/"
+LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
